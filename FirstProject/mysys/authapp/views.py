87bd462760b5d6ab519ccecc3210d2d9554d2367
hasibrafi <hasibulislam405@gmail.com>
2019-10-01 16:50:29 +0600

    template added

--- FirstProject/mysys/authapp/views.py
+++ FirstProject/mysys/authapp/views.py
@@ -4,4 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello World")
+    my_dict = {'insert_me':"Hello I am Hasib Rafi.And I am from authapp/index.html"}
+    return render(request,'authapp/index.html',context=my_dict)
+    #return HttpResponse("Hello World")
