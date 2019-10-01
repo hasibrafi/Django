f3197373a0f349947e37742b4ba30a6cb8653dc0
hasibrafi <hasibulislam405@gmail.com>
2019-10-01 16:07:11 +0600

    include() function added

--- FirstProject/mysys/mysys/urls.py
+++ FirstProject/mysys/mysys/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include
 from authapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('authapp/',include('authapp.urls')),
     path('index',views.index,name="index"),
 ]
