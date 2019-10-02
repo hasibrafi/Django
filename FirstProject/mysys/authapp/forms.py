b89898168fa67c269c3a8dc2d6659060bcb45f51
hasibrafi <hasibulislam405@gmail.com>
2019-10-02 17:49:10 +0600

    Model created and migrated

--- FirstProject/mysys/authapp/forms.py
+++ FirstProject/mysys/authapp/forms.py
@@ -1,4 +1,19 @@
 from django import forms
+from django.contrib.auth.models import User
+from authapp.models import UserProfileInfo
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta():
+        model = User
+        fields = ('username','email','password')
+
+# class UserProfileInfo(forms.ModelForm):
+#     class Meta():
+#         model = UserProfileInfo
+#         fields = ''
+
 
 # class SignUp(forms.Form):
 #     username = forms.CharField()
--- FirstProject/mysys/authapp/models.py
+++ FirstProject/mysys/authapp/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-# from django.contrib.auth.models import User
-# # Create your models here.
-#
-# class UserPortfolioInfo(models.Model):
-#     user = models.OneToOneField(User)
-#
-#     def __str__(self):
-#         return self.user.username
+from django.contrib.auth.models import User
+# Create your models here.
+
+class UserProfileInfo(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
